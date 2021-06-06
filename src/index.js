3e8b53e721f11e9f4581905d5f0e969df9277f8e
Binh Pham <pltbinh@gmail.com>
2021-06-06 20:29:02 +0700

    Implement basic error boundary

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import ErrorBoundary from './ErrorBoundary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary
+      fallbackRender =  {({error, resetErrorBoundary, componentStack}) => (
+          <div>
+          <h1>An error occurred: {error.message}</h1>
+          <button onClick={resetErrorBoundary}>Try again</button>
+        </div>
+      )}
+    >
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
